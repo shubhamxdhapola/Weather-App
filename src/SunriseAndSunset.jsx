ae2299ec3dae67de54e77d5501fd4761dae79074
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-02-04 01:18:38 +0530

    Changes in styles

--- src/SunriseAndSunset.jsx
+++ src/SunriseAndSunset.jsx
@@ -1,15 +1,17 @@
+import WbTwilightIcon from '@mui/icons-material/WbTwilight';
 export default function SunriseAndSunset({weatherInfo}) {
     
     return(
         <>
             <div className="sunrise-and-sunset">
-                <div className="sunrise sun">
-                    <p className="sunrise-text sun-text"><i className="fa-solid fa-arrow-up"></i> Sunrise</p>
-                    <p className="sunrise-time sun-time">{weatherInfo.sunriseTime.split(',')[2]}</p>
+                <div className="sunrise">
+                    <p className="sun-text"><i className="fa-solid fa-arrow-up"></i> Sunrise</p>
+                    <p className=" sun-time">{weatherInfo.sunriseTime.split(',')[2]}</p>
                 </div>
-                <div className="sunset sun">
-                    <p className="sunrise-text sun-text">Sunset <i className="fa-solid fa-arrow-down"></i> </p>
-                    <p className="sunrise-time sun-time">{weatherInfo.sunsetTime.split(',')[2]}</p>
+                <WbTwilightIcon className='sun-icon'/>
+                <div className="sunset">
+                    <p className=" sun-text">Sunset <i className="fa-solid fa-arrow-down"></i> </p>
+                    <p className="e sun-time">{weatherInfo.sunsetTime.split(',')[2]}</p>
                 </div>
             </div>
         </>
